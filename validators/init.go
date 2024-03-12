e26c4438764a247066a9a79a36beea188eabccd7
json <fxj@acewill.cn>
2024-03-12 23:20:18 +0800

    删掉自定义验证

--- validators/init.go
+++ validators/init.go
@@ -5,16 +5,13 @@ import (
 
 	"github.com/gin-gonic/gin/binding"
 	ut "github.com/go-playground/universal-translator"
-	"github.com/go-playground/validator/v10"
 )
 
 var Trans ut.Translator
 
 func RegisterValidators() {
 	binding.Validator = &defaultValidator{}
-	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		v.RegisterValidation("sample", sample)
-	}
+
 }
 
 func Validate(obj any) error {
--- validators/sample.go
+++
@@ -1,9 +0,0 @@
-package validators
-
-import (
-	"github.com/go-playground/validator/v10"
-)
-
-var sample validator.Func = func(fl validator.FieldLevel) bool {
-	return false
-}
