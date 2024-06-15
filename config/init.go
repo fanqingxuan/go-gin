8f0801974834e8358e90bfb4edc5e0b4af8655b0
json <fxj@acewill.cn>
2024-06-15 23:07:54 +0800

    整理main.go文件

--- config/init.go
+++ config/init.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	filex "go-gin/internal/file"
 )
 
@@ -9,7 +8,6 @@ var instance Config
 
 func Init(filename string) {
 	filex.MustLoad(filename, &instance)
-	fmt.Println(instance)
 }
 
 func IsDebugMode() bool {
