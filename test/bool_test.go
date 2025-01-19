62a414c30496d56d90d4ac75fc5b6e219d5baed2
json <fxj@acewill.cn>
2025-01-19 09:42:08 +0800

    引入stretchr/testify测试库

---
+++ test/bool_test.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"go-gin/util"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsTrue(t *testing.T) {
+	assert.True(t, util.IsTrue(11), "number not zero should be true")
+	assert.True(t, util.IsFalse(0), "number zero should be false")
+	assert.True(t, util.IsTrue("hello"), "string should be true")
+	assert.True(t, util.IsFalse(""), "empty string should be false")
+	assert.True(t, util.IsTrue(1.0), "float number should be true")
+	assert.True(t, util.IsFalse(0.0), "float number should be false")
+}
+
+func TestConditional(t *testing.T) {
+
+	assert.Equal(t, util.Conditional(true, "hello", ""), "hello", "condtional should return first value")
+	assert.Equal(t, util.Conditional(false, "hello", "aa"), "aa", "condtional should return second value")
+}
